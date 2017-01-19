82bae640145fbe3d19214a9969178767feb0d9f9
Max Jonas Werner <mail@makk.es>
2017-01-19 23:50:09 +0100

    Added package documentation; improved existing doc

--- try.go
+++ try.go
@@ -1,3 +1,5 @@
+// Package justlib contains handy utility functions to make your day-to-day Go
+// programming more pleasant.
 package justlib
 
 import (
@@ -5,11 +7,10 @@ import (
 	"time"
 )
 
-// Try calls the given function f at most »times« times as long as it returns
-// an error, sleeping the given backoffTime in between subsequent calls.  If f
-// returns an error on the last try, that error is returned by Try.
-// If »times« is 0, then »f« is called indefinitely as long as it returns an
-// error.
+// Try calls the given function »f« at most »times« times as long as it returns
+// an error, sleeping the given »backoffTime« in between subsequent calls.  If
+// f returns an error on the last try, that error is returned by Try.
+// If times is 0, then f is called indefinitely as long as it returns an error.
 func Try(times uint16, backoffTime time.Duration, f func() error) error {
 	var err error
 	var i uint16
