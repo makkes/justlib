169d19eb7958b4f2183137a810335a80e27e5360
Max Jonas Werner <max@just.social>
2017-01-25 00:32:13 +0100

    Worker: Fixed example

--- work/worker_test.go
+++ work/worker_test.go
@@ -6,15 +6,16 @@ import "time"
 import "github.com/justsocialapps/justlib/work"
 
 // this is our worker function that is called for every new job
-func f(p work.Payload) {
+func f(p work.Payload) interface{} {
 	fmt.Println(p.Data)
 	time.Sleep(200 * time.Millisecond)
+	return nil
 }
 
 func Example() {
 
 	// create a worker with 3 goroutines that can process jobs in parallel
-	worker := work.NewWorker(3, f)
+	worker := work.NewWorker(3, f, false)
 
 	// create 100 jobs and dispatch them to the worker.
 	for i := 0; i < 100; i++ {
